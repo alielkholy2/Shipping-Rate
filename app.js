017aa8ef723accf57cf7a87ba3f414bfd778a07c
alielkholy2 <74504075+alielkholy2@users.noreply.github.com>
2020-11-15 20:45:58 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let theInput = (document.getElementById("input").oninput = function () {
+  let theResult = Math.floor((this.value / 14) * 10);
+  console.log(typeof theResult);
+  let result = document.getElementById("result");
+
+  if (isNaN(this.value)) {
+    result.textContent = `متكتب يبني رقم  `;
+document.getElementById('xy').play();
+           document.getElementById('xyz').pause();
+
+  } else {
+    result.textContent = `هيوصلك ${theResult}  جنيه `;
+  }
+  if (this.value >= 1000000) {
+    result.textContent = `😂😂 بتجرب رقم معدي المليون  يبني فوق  انت كحياااان `;
+  }
+  if (this.value < 0) {
+    result.textContent = ` ازي بقي انا عاوز حضرتك تفهني ازي`;
+  }
+   if(this.value >= 1000000)
+{
+    document.getElementById('xyz').play();
+     document.getElementById('xy').pause();
+    
+}
+    if (this.value >= 100) {
+document.getElementById('img').play();
+  }
+ 
+});
+
+
+
